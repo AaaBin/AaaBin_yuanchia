a94473e41574fe812f727eadca411d50c2427e0e
AaaBin <52748728+AaaBin@users.noreply.github.com>
2020-03-20 14:26:56 +0800

    2020-03-20-1430

--- 01-個人專題/05-js/v1.js
+++ 01-個人專題/05-js/v1.js
@@ -1,20 +1,8 @@
-let thumbs_bgs = document.querySelectorAll('.product_thumb img');
-        // console.log(thumbs_bgs);
-        // console.log(thumbs_bgs[0]);
-        thumbs_bgs.forEach(thumbs_bg => {
-            thumbs_bg.onmouseover = function(){
-                    thumbs_bg.setAttribute('src','../01-img/測試用圖片/芝芝葡萄1.png')
-            }
-            thumbs_bg.onmouseout = function(){
-                    thumbs_bg.setAttribute('src','../01-img/測試用圖片/草莓奶酪.png')
-                }
-        });
-
 // about_us的折疊內容，當有任一內容被顯示時，隱藏其他內容
 $('.about_us_feature').on('show.bs.collapse', function () {
         $('.about_us_feature article').collapse('hide');
 })
-
+// about_us區塊的圖片切換
 $('#feature1').on('shown.bs.collapse', function () {
         $('.about_us_img_area img').attr('src','../01-img/測試用圖片/草莓奶酪.png');
 })
@@ -27,3 +15,20 @@ $('#feature3').on('shown.bs.collapse', function () {
 $('#feature4').on('shown.bs.collapse', function () {
         $('.about_us_img_area img').attr('src','../01-img/測試用圖片/test.jpg');
 })
+
+
+
+
+// pruduct區塊的下方縮圖
+let thumbs_bgs = document.querySelectorAll('.product_thumb img');
+        // console.log(thumbs_bgs);
+        // console.log(thumbs_bgs[0]);
+        thumbs_bgs.forEach(thumbs_bg => {
+            thumbs_bg.onmouseover = function(){
+                    thumbs_bg.setAttribute('src','../01-img/測試用圖片/芝芝葡萄1.png')
+            }
+            thumbs_bg.onmouseout = function(){
+                    thumbs_bg.setAttribute('src','../01-img/測試用圖片/草莓奶酪.png')
+                }
+        });
+
